6e08e26a6e52e9512e51a5b1edb1a824743b3aab
Killg0d <shettytarun2002@gmail.com>
2024-01-16 08:51:00 +0530

    All done

--- client_test.py
+++ client_test.py
@@ -1,5 +1,5 @@
 import unittest
-from client3 import getDataPoint
+from client3 import getDataPoint,getRatio
 
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
@@ -8,6 +8,8 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote) , (quote["stock"], quote["top_bid"]["price"],quote["top_ask"]["price"],(quote["top_bid"]["price"]+quote["top_ask"]["price"])/2))
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,11 +17,31 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
-
+    for quote in quotes:
+      self.assertEqual(getDataPoint(quote) , (quote["stock"], quote["top_bid"]["price"],quote["top_ask"]["price"],(quote["top_bid"]["price"]+quote["top_ask"]["price"])/2))
 
   """ ------------ Add more unit tests ------------ """
+  def test_getRatio_normal(self):
+    # Test cases with non-zero price_b
+    test_cases = [
+        {"price_a": 120.48, "price_b": 119.2},
+        {"price_a": 117.87, "price_b": 121.68}
+    ]
 
+    for case in test_cases:
+        with self.subTest(case=case):
+            result = getRatio(case["price_a"], case["price_b"])
+            self.assertEqual(result,case["price_a"]/case["price_b"])
 
+  def test_getRatio_price_b_zero(self):
+    # Test case where price_b is zero
+    result = getRatio(120.48, 0)
+    self.assertIsNone(result)
+
+  def test_getRatio_price_a_zero(self):
+     # Test case where price_a is zero
+     test_case={"price_a": 0, "price_b": 119.2}
+     self.assertEqual(getRatio(test_case["price_a"],test_case['price_b']),test_case['price_a']/test_case['price_b'])
 
 if __name__ == '__main__':
     unittest.main()
